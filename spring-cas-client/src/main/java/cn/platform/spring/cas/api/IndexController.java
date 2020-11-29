0aadf1d8b24b6fec65a1fbbf2a8739931ea2f6af
聂冬佳 <ndj@newgrand.cn>
2020-11-29 22:28:38 +0800

    <feature>:<前后端分离-调用接口测试>

--- spring-cas-client/src/main/java/cn/platform/spring/cas/api/IndexController.java
+++ spring-cas-client/src/main/java/cn/platform/spring/cas/api/IndexController.java
@@ -9,6 +9,7 @@
 package cn.platform.spring.cas.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +30,7 @@ public class IndexController {
         return time.toString();
     }
     // 获取当前用户
-    @GetMapping(value = "current-user-info")
+    @PostMapping(value = "current-user-info")
     public Object getCurrentUserInfo() {
         LocalDateTime time = LocalDateTime.now();
         return time.toString();
