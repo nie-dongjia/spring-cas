5c7d1d07a76769c7ee595f4818530d18822e4a3f
聂冬佳 <ndj@newgrand.cn>
2020-12-01 21:24:05 +0800

    <feature>:<client-three构建>

---
+++ spring-cas-client-three/src/main/java/cn/platform/spring/cas/CasClientThreeApplication.java
@@ -0,0 +1,21 @@
+package cn.platform.spring.cas;
+
+import net.unicon.cas.client.configuration.EnableCasClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+/**
+ * CasClientApplication 启动类
+ *
+ * @author op.nie-dongjia
+ * @date 2016/10/31
+ */
+@ServletComponentScan
+@SpringBootApplication
+@EnableCasClient
+public class CasClientThreeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CasClientThreeApplication.class, args);
+    }
+}
---
+++ spring-cas-client-three/src/main/java/cn/platform/spring/cas/api/IndexController.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2020-2025. zntie.com All Rights Reserved.
+ * 项目名称：
+ * 类名称：OnlineRatingController.java
+ * 创建人：chen aotu
+ * 联系方式：niedongjiamail@qq.com
+ * 项目官网:
+ */
+package cn.platform.spring.cas.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+/**
+ * 测试接口
+ *
+ * @author op.nie-dongjia
+ * @date 2016/10/31
+ */
+@RequestMapping("/api/index/")
+@RestController
+public class IndexController {
+    @GetMapping(value = "current-time")
+    public Object realTime() {
+        LocalDateTime time = LocalDateTime.now();
+        return time.toString();
+    }
+    // 获取当前用户
+    @GetMapping(value = "current-user-info")
+    public Object getCurrentUserInfo() {
+        LocalDateTime time = LocalDateTime.now();
+        return time.toString();
+    }
+
+}
